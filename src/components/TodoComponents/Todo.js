ff1ef8a03acb574276a0b92e627467c747892c03
ItelSunday <iteldomingotech@gmail.com>
2019-02-13 15:32:44 -0800

    added objects

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Todo(props) {
+    return (
+        <div className= "test"></div>
+    );
+}
+
+export default Todo;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoForm = props => {
+    return (
+        <form>
+            {/* <input></input>
+            <input></input> */}
+            {/* <button type ="submit">Add To Do</button>
+            <button type ="reset">Clear Completed</button> */}
+        </form>
+    );
+}
+
+export default TodoForm;
